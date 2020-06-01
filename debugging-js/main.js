774bdfb36abf845a1b3f28c0b155f71ce45c6b90
Amy Gori <amydgori@gmail.com>
2020-06-01 15:43:32 -0400

    Add this linter-fixed bug back into example

--- debugging-js/main.js
+++ debugging-js/main.js
@@ -18,7 +18,7 @@ function checkGuess () {
   }
   guesses.textContent += userGuess + ' '
 
-  if (userGuess === randomNumber) {
+  if (userGuess = randomNumber) {
     lastResult.textContent = 'Congratulations! You got it right!'
     lastResult.style.backgroundColor = 'green'
     lowOrHi.textContent = ''
