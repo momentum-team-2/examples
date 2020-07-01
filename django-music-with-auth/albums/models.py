3aa103f804f5ef5cc406d80d9b214f0e8cb7337e
Amy Gori <amydgori@gmail.com>
2020-07-01 14:13:24 -0400

    Add m2m relationship with Genre

--- django-music-with-auth/albums/models.py
+++ django-music-with-auth/albums/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+
 class Album(models.Model):
     YEARS = [(i, i) for i in range(1900, 2100)]
 
     title = models.CharField(max_length=255)
     artist_name = models.CharField(max_length=255)
     year = models.IntegerField(choices=YEARS)
+    genres = models.ManyToManyField(Genre, related_name="albums")
 
     def __str__(self):
         return self.title
